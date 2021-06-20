97f59ac0bc7487a746a895381f2fdcbe3a16afb1
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2021-06-19 20:12:14 -0700

    First iteration

---
+++ script.js
@@ -0,0 +1,19 @@
+const openMenu = document.querySelector('.nav_lines');
+const navUL = document.querySelector('.nav-ul');
+let menuOpen = false;
+
+
+openMenu.addEventListener('click', () =>{
+
+    if(menuOpen == false){
+        navUL.style.top = '0%';
+        
+        menuOpen = true;
+    } else {
+        navUL.style.top = '-100%';
+        menuOpen = false;
+    }
+});
+
+
+
