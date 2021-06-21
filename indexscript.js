42e07e0d6016a0df220ccca1d609d2bc3388594a
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2021-06-21 11:13:15 -0700

    iteration 2
    added homepage

---
+++ indexscript.js
@@ -0,0 +1,5 @@
+const clickScreen = document.getElementById("clickScreen");
+
+clickScreen.addEventListener("click", () => {
+  window.location.href = "portfolio.html";
+});
--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const openMenu = document.querySelector('.nav_lines');
-const navUL = document.querySelector('.nav-ul');
+const openMenu = document.querySelector(".nav_lines");
+const navUL = document.querySelector(".nav-ul");
 let menuOpen = false;
 
+openMenu.addEventListener("click", () => {
+  if (menuOpen == false) {
+    navUL.style.top = "0%";
 
-openMenu.addEventListener('click', () =>{
-
-    if(menuOpen == false){
-        navUL.style.top = '0%';
-        
-        menuOpen = true;
-    } else {
-        navUL.style.top = '-100%';
-        menuOpen = false;
-    }
+    menuOpen = true;
+  } else {
+    navUL.style.top = "-100%";
+    menuOpen = false;
+  }
 });
-
-
-
