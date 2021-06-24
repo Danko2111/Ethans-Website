6e63d355776ed373aa341a1c92752bb514003fd9
Danko2111 <82421874+Danko2111@users.noreply.github.com>
2021-06-23 22:31:51 -0700

    newest adds

--- indexscript.js
+++ indexscript.js
@@ -1,8 +1,12 @@
 const clickScreen = document.getElementById("clickScreen");
 const divCard = document.getElementById("card");
 const cardContainer = document.getElementById("container");
+const cardbutton = document.getElementById("cardbutton");
+const logo = document.getElementById("logo");
 
 clickScreen.addEventListener("click", () => {
   divCard.className += " expand";
   cardContainer.className += " playanim";
+  logo.className += " close";
+  cardbutton.className += " close";
 });
